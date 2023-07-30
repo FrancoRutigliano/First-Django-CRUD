3d34f010d723a582f86451a531547b721ae042dd
Franco Rutigliano <francorutigliano11@gmail.com>
2023-07-30 09:36:02 -0300

    Template de Tareas - Agregando manejo de errores de integridad con la DB

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.http import HttpResponse
+from django.contrib.auth import login
+from django.db import IntegrityError
 
 
 def home(request):
@@ -17,18 +18,22 @@ def signup(request):
     else:
         if request.POST['password1'] == request.POST['password2']:
             try:
-                # register user
                 user = User.objects.create_user(
                     username=request.POST['username'], password=request.POST['password1'])
                 user.save()
-                return HttpResponse('Usuario creado satisfactoriamente')
-            except:
+                login(request, user)
+                return redirect('tasks')
+            except IntegrityError:
                 return render(request, 'signup.html', {
                     'form': UserCreationForm,
-                    'error': 'Este usuario actualmente ya existe.'
+                    'error': 'El usuario ya existe'
                 })
 
         return render(request, 'signup.html', {
             'form': UserCreationForm,
             'error': 'Las contraseñas no coinciden.'
         })
+
+
+def tasks(request):
+    return render(request, 'tasks.html')
